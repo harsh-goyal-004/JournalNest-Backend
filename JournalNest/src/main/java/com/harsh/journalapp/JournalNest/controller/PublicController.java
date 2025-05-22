1cc86613ee48a00a3de20ab9c739aa984a75e709
Harsh Goyal <harshgoyal2893@gmail.com>
2025-05-23 00:29:09 +0530

    Initial Commit

---
+++ JournalNest/src/main/java/com/harsh/journalapp/JournalNest/controller/PublicController.java
@@ -0,0 +1,16 @@
+package com.harsh.journalapp.JournalNest.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PublicController {
+
+    @GetMapping("/health-check")
+    public String healthCheck(){
+        return "OK";
+    }
+}
